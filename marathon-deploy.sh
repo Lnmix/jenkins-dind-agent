9d53e39f3d79e777eb1d3700d4886409ff2aa853
Max V <max@leadnomics.com>
2016-11-09 12:30:55 -0500

    more jq wizardry

--- marathon-deploy.sh
+++ marathon-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 APP_DEFINITION=$(jq -Mcrs 'add' $1 -)
-APP_ID=$(echo $APP_DEFINITION | jq -Mcrs '.id')
+APP_ID=$(echo $APP_DEFINITION | jq -Mcr '.id')
 echo $APP_DEFINITION | curl --silent --fail http://master.mesos:8080/v2/apps/$APP_ID -H 'Content-Type: application/json' -X PUT -d @-
 exit $?
